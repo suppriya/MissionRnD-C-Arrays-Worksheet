0856984c97144b832a4c4c28b6c2263fa75d1888
suppriya <suppriya@missionrnd.com>
2016-03-18 04:10:37 -0700

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -11,6 +11,7 @@ OUTPUT: Return top K students from array. Output need not be in decreasing order
 ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
+
 */
 
 #include <iostream>
@@ -20,7 +21,48 @@ struct student {
 	char *name;
 	int score;
 };
+int i, m;
+
 
 struct student ** topKStudents(struct student *students, int len, int K) {
+	if ((students != NULL) && (len > 0) && (K > 0))
+	{
+		struct student **ans = (struct student**)malloc(sizeof(struct student**)*K);
+		struct student *temp = (struct student*)malloc(sizeof(struct student*)*len);
+		
+		for (int i = 0; i < len; i++)
+		{
+			for (int j = i + 1; j < len; j++)
+			{
+				if (students[i].score > students[j].score)
+				{
+					temp[i] = students[i];
+					students[i] = students[j];
+					students[j] = temp[i];
+				}
+			}
+		}
+		if (K <= len) 
+		{
+			m = 1;
+			int p = 0;
+			for (int j = len ; j>= 1; j--)
+			{
+				if (m <= K)
+				{
+					ans[p++] = &students[j-1];
+				}
+				m++;
+			}
+		}
+		else if (K > len)
+		{
+			for (int i = 0; i < len; i++)
+			{
+				ans[i] = &students[i];
+			}
+		}
+		return ans;
+	}
 	return NULL;
 }
